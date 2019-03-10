8e71b22334b2c399fb76f3debf22ec170e721ad8
Dylan White <dylanawhite11@gmail.com>
2019-03-10 16:29:01 -0500

    creating and modularizing componenets

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import Button from "./Button"
+import Buttons from "./Buttons"
+import Display from "./Display"
+import Logo from "./Logo"
+
+export {
+    Button,
+    Buttons,
+    Display,
+    Logo
+}
