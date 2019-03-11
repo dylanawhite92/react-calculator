c86951f2c958f89a448bbdd749891ded8e2629f2
Dylan White <dylanawhite11@gmail.com>
2019-03-11 15:04:22 -0500

    binding click handlers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import "./App.css"
-import { Button, Input } from "./components/index"
+import { Button, Input, ClearButton } from "./components/index"
 
 class App extends Component {
   constructor(props) {
@@ -11,34 +11,41 @@ class App extends Component {
     }
   }
 
+  handleInput = val => {
+    this.setState({ input: this.state.input + val });
+  }
+
   render() {
     return (
       <div className="app">
         <div className="calc-wrapper">
           <Input input={this.state.input}/>
           <div className="row">
-            <Button>7</Button>
-            <Button>8</Button>
-            <Button>9</Button>
-            <Button>/</Button>
+            <Button handleClick={this.handleInput}>7</Button>
+            <Button handleClick={this.handleInput}>8</Button>
+            <Button handleClick={this.handleInput}>9</Button>
+            <Button handleClick={this.handleInput}>/</Button>
           </div>
           <div className="row">
-            <Button>4</Button>
-            <Button>5</Button>
-            <Button>6</Button>
-            <Button>X</Button>
+            <Button handleClick={this.handleInput}>4</Button>
+            <Button handleClick={this.handleInput}>5</Button>
+            <Button handleClick={this.handleInput}>6</Button>
+            <Button handleClick={this.handleInput}>X</Button>
           </div>
           <div className="row">
-            <Button>1</Button>
-            <Button>2</Button>
-            <Button>3</Button>
-            <Button>+</Button>
+            <Button handleClick={this.handleInput}>1</Button>
+            <Button handleClick={this.handleInput}>2</Button>
+            <Button handleClick={this.handleInput}>3</Button>
+            <Button handleClick={this.handleInput}>+</Button>
           </div>
           <div className="row">
-            <Button>.</Button>
-            <Button>0</Button>
-            <Button>=</Button>
-            <Button>-</Button>
+            <Button handleClick={this.handleInput}>.</Button>
+            <Button handleClick={this.handleInput}>0</Button>
+            <Button handleClick={this.handleInput}>=</Button>
+            <Button handleClick={this.handleInput}>-</Button>
+          </div>
+          <div className="row">
+            <ClearButton handleClear={() => this.setState({input: "0"})}>Clear</ClearButton>
           </div>
         </div>
       </div>
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import { Button } from "./Button/Button";
 import { Input } from "./Input/Input";
+import { ClearButton } from "./ClearButton/ClearButton";
 
 export {
     Button,
     Input,
+    ClearButton
 }
