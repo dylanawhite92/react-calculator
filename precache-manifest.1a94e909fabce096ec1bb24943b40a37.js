477e41e18dedc9798731e9b99aae27cf61f50835
Dylan White <dylanawhite11@gmail.com>
2019-03-13 19:13:17 -0500

    Updates

---
+++ precache-manifest.1a94e909fabce096ec1bb24943b40a37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "012c32b99d991f5cba3a",
+    "url": "/react-calculator/static/css/main.0f0d967c.chunk.css"
+  },
+  {
+    "revision": "012c32b99d991f5cba3a",
+    "url": "/react-calculator/static/js/main.42260404.chunk.js"
+  },
+  {
+    "revision": "6473c9243ea43aad7509",
+    "url": "/react-calculator/static/js/runtime~main.30fd8698.js"
+  },
+  {
+    "revision": "0f6606d633c1d047ed47",
+    "url": "/react-calculator/static/js/2.c4ae4785.chunk.js"
+  },
+  {
+    "revision": "d13ad076e9444d65b4b6d829bde9b6a2",
+    "url": "/react-calculator/index.html"
+  }
+];
--- src/components/ClearButton/ClearButton.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./ClearButton.css";
-
-// Button that handles clearing the Input display
-export const ClearButton = props => (
-    <div className="clear-btn" onClick={props.handleClear}>{props.children}</div>
-);
